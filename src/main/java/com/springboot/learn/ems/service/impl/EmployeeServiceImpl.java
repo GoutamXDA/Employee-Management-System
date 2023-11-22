b2e8c072cb47e166c58be88ce6c6bbc4d060f7b6
Goutamvante <goutamvant@gmail.com>
2023-11-22 11:03:50 +0530

    Implementation of getById

--- src/main/java/com/springboot/learn/ems/service/impl/EmployeeServiceImpl.java
+++ src/main/java/com/springboot/learn/ems/service/impl/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Service;
 
 import com.springboot.learn.ems.dto.EmployeeDto;
 import com.springboot.learn.ems.entity.Employee;
+import com.springboot.learn.ems.exception.ResourceNotFoundException;
 import com.springboot.learn.ems.mapper.EmployeeMapper;
 import com.springboot.learn.ems.repository.EmployeeRepository;
 import com.springboot.learn.ems.service.EmployeeService;
@@ -13,7 +14,7 @@ import lombok.AllArgsConstructor;
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
-
+private static final String EMPLOYEE_NOT_FOUND="Employee is not exists with given id";
 	private EmployeeRepository employeeRepository;
 
 	@Override
@@ -22,6 +23,14 @@ public class EmployeeServiceImpl implements EmployeeService {
 		Employee employee = EmployeeMapper.maptoEmployee(employeeDto);
 		Employee savedEmployee = employeeRepository.save(employee);
 		return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+	} 
+
+	@Override
+	public EmployeeDto getEmployeeById(Long employeeId) {
+		Employee employee = employeeRepository.findById(employeeId)
+		.orElseThrow(()-> new ResourceNotFoundException(EMPLOYEE_NOT_FOUND+":"+employeeId));
+		
+		return EmployeeMapper.mapToEmployeeDto(employee);
 	}
 
 }
